2262174acf8a00df6bf4072970aed5ec48b7a6a6
LeonoffNSU <k.leonov@g.nsu.ru>
2023-05-13 19:03:04 +0700

    begin

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ budget = 8000
 # day 2
 print(ru.phrase2_1)
 interactive2_1 = input(ru.phrase2_2)
-interactive2_1.strip('!?,.:;-_/ ')
+interactive2_1 = interactive2_1.strip('!?,.:;-_/ ')
 if interactive2_1 == 'да' or interactive2_1 == 'yes':
-    print('a')
+    interactive2_1_1 = input(ru.phrase2_2_1)
+    interactive2_1_1 = interactive2_1_1.strip('!?,.:;-_/ ')
+    if interactive2_1_1 == '1':
+        print(ru.phrase_singly)
+    interactive2_1_2 = input(ru.phrase2_2_2)
+    interactive2_1_2 = interactive2_1_2.strip('!?,.:;-_/ ')
+    if interactive2_1_2 == 'да' or interactive2_1_2 == 'yes':
+        print('круто')
+    else:
+        print(ru.phrase_refusing)
