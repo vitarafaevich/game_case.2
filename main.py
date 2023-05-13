529d9bd891360352fbf281d49b5783a523ffcc90
LeonoffNSU <k.leonov@g.nsu.ru>
2023-05-13 18:39:51 +0700

    begin

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import ru_local as ru
+budget = 8000
 
+# day 2
+print(ru.phrase2_1)
+interactive2_1 = input(ru.phrase2_2)
+interactive2_1.strip('!?,.:;-_/ ')
+if interactive2_1 == 'да' or interactive2_1 == 'yes':
+    print('a')
