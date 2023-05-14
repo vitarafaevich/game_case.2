b8bdde9eeaef37efca2c0dc8c7ca8625318a42ed
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-05-14 20:42:48 +0700

    day 1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import ru_local as ru
 import time
-budget = 8000
+budget = 16000
 a = input(ru.BEGIN)
 print('Day 1')
 answer1_1 = input(ru.PHRASE1_1)
@@ -116,53 +116,65 @@ if interactive2_1 == 'да' or interactive2_1 == 'yes':
 #day 3
 print('Day 3')
 time.sleep(1.5)
-a_ = input(ru.DAY3_Q1)
+print(ru.DAY3_Q1)
 budget = budget - 2500
 print(ru.PHRASE1_3, budget, ru.RUBLES)
-time.sleep(1.5)
+time.sleep(1.0)
 b_ = input(ru.DAY3_Q2)
 if b_.lower() == ru.NO or b_.lower() == 'no':
     x_ = input(ru.DAY3_Q2_NO)
-    time.sleep(1.5)
+    time.sleep(2.5)
     print('Day 4')
-    time.sleep(1.5)
+    time.sleep(2.5)
     y_ = input(ru.DAY4_Q1_1)
 if b_.lower() == ru.YES or b_.lower() == 'yes':
-    c_1 = input(ru.DAY3_Q2_YES_1_1)
-    c_2 = input(ru.DAY3_Q2_YES_1_2)
-    c_3 = input(ru.DAY3_Q2_YES_1_3)
-    answer_q3 = input(ru.DAY3_Q3_O)
-    if '1' in answer_q3:
-        e_ = input(ru.DAY3_Q3_1)
-        budget = budget - 5000
-        time.sleep(1.5)
-        print(ru.PHRASE1_3, budget, ru.RUBLES)
-        time.sleep(1.5)
-        f_ = input(ru.DAY3_Q3_1_2)
-        g_ = input(ru.DAY3_Q3_1_3)
-        h_ = input(ru.DAY3_Q3_1_4)
-        i_ = input(ru.DAY3_Q3_1_5)
-        #СДЛЕАТЬ ЦИКЛ ВОЗВРАЩЕНИЯ К ВОПРОСУ И ВОЗРАТА 5К
-    if '2' in answer_q3:
-        j_ = input(ru.DAY3_Q3_2_1)
-        k_ = input(ru.DAY3_Q3_2_2)
-        if answer1_5.lower() == ru.YES or answer1_5.lower() == 'yes':
-            budget = budget - 2500
-            print(ru.PHRASE1_3, budget, ru.RUBLES)
-            time.sleep(1.0)
-            l_ = input(ru.DAY3_Q4_1_1)
-            m_ = input(ru.DAY3_Q4_1_2)
-        if answer1_5.lower() == ru.NO or answer1_5.lower() == 'no':
-            budget = budget - 5000
-            print(ru.PHRASE1_3, budget, ru.RUBLES)
-            time.sleep(1.0)
-            n_ = input(ru.DAY3_Q5_1_1)
-            m_
-    o_ = input(ru.DAY3_Q6_1_1)
+    print(ru.DAY3_Q2_YES_1_1)
+    time.sleep(1.0)
+    print(ru.PHRASE3_5)
+    time.sleep(2.0)
+    print(ru.DAY3_Q2_YES_1_2)
+    time.sleep(2.0)
+    while True:
+        answer_q3 = input(ru.DAY3_Q2_YES_1_3)
+        try:
+            if '1' in answer_q3:
+                print(ru.DAY3_Q3_1)
+                budget = budget - 5000
+                print(ru.PHRASE1_3, budget, ru.RUBLES)
+                print(ru.PHRASE3_13)
+                time.sleep(2.5)
+                print(ru.DAY3_Q3_1_2)
+                print(ru.DAY3_Q3_1_3)
+                time.sleep(2.0)
+                print(ru.DAY3_Q3_1_4)
+                time.sleep(2.0)
+                print(ru.DAY3_Q3_1_5)
+            else:
+                j_ = input(ru.DAY3_Q3_2_1)
+                k_ = input(ru.DAY3_Q3_2_2)
+                if answer1_5.lower() == ru.YES or answer1_5.lower() == 'yes':
+                    budget = budget - 2500
+                    print(ru.PHRASE1_3, budget, ru.RUBLES)
+                    time.sleep(1.0)
+                    l_ = input(ru.DAY3_Q4_1_1)
+                    m_ = input(ru.DAY3_Q4_1_2)
+                if answer1_5.lower() == ru.NO or answer1_5.lower() == 'no':
+                    budget = budget - 5000
+                    print(ru.PHRASE1_3, budget, ru.RUBLES)
+                    time.sleep(1.0)
+                    n_ = input(ru.DAY3_Q5_1_1)
+                    m_
+                break
+        except:
+            if answer_q3.isalpha():
+                print('try')
+    print(ru.DAY3_Q6_1_1)
+    time.sleep(1.5)
+    print(ru.PHRASE3_23)
     budget = budget - 400
     print(ru.DAY3_Q6_1_3, ru.PHRASE1_3, budget, ru.RUBLES)
-    time.sleep(1.0)
-    p_ = input(ru.DAY3_Q6_1_2)
+    time.sleep(2.0)
+    print(ru.DAY3_Q6_1_2)
     time.sleep(1.5)
     print('Day 4')
     time.sleep(1.5)
