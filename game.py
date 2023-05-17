0cbc4b000193e8cb6157aa215f487f5a891e2542
LeonoffNSU <k.leonov@g.nsu.ru>
2023-05-17 17:13:18 +0700

    begin

--- game.py
+++ game.py
@@ -6,10 +6,18 @@ GAME_0 = 'Если ответ верный, введите "1", иначе "0"'
 
 print(GAME_1)
 print(GAME_0)
-z_0 = input()
-ans_1 = input('25*3>6*10 + 5')
-if time.sleep<=(15) and '1' in ans_1:
-    ans_2 = input('436+416=852')
-    if '2' in ans_2:
-        ans_3 = input('')
-else: print('Game over :(')
+ans_1 = input('25*3 > 6*10 + 5')
+counter = 0
+if time.sleep <= (12) and '1' in ans_1:
+    counter += 1
+ans_2 = input('436+416=852')
+if time.sleep <= (12) and '1' in ans_2:
+    counter += 1
+ans_3 = input('')
+else:
+    print('Game over :(')
+
+ans_3 = '45*13 = 586'
+ans_5 = 'x^2 - 144 = 0 при x = 12'
+ans_6 = 'lg 100 = 10'
+ans_7 = ''
